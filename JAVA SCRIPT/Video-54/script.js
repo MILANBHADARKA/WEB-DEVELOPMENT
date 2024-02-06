ae17633aae95513274af3c3fd1477ad24a540a73
MILAN BHADARKA <bhadarkamilan3@gmail.com>
2024-02-06 22:05:21 +0530

    06-01-2024

---
+++ JAVA SCRIPT/Video-54/script.js
@@ -0,0 +1,19 @@
+alert("Welcome to the website");
+
+console.log("code is running");
+console.log("code is also running");
+console.log("code is also also running");
+
+var a = prompt("enter your number");
+var istrue=confirm("are you want to leave this page");
+
+if(istrue)
+{
+    console.log("byy");
+}
+else
+{
+    console.log("hi");
+}
+
+console.log("your number is " +a);
