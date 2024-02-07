463119ca0e95378eccdf9341689336f1fab08145
MILAN BHADARKA <bhadarkamilan3@gmail.com>
2024-02-07 09:45:18 +0530

    07-02-2024

---
+++ JAVA SCRIPT/Video-55/script.js
@@ -0,0 +1,50 @@
+console.log("Hey how are you?");
+
+var a=5;                //all the rule for variable name is like c.
+var b=7.5;               //in javascript variable name is case sensitive means a and A is different variable.
+var c="milan";    
+
+console.log(a+b);
+console.log(c);
+a=a+10;
+console.log(a);
+
+console.log(typeof a,typeof b,typeof c);      //for print type of any variable.
+
+const a1=9;        //now we cannot change value of a1.
+// a1=a1+10;      //this is an error.
+
+let z=5;
+let v="hello";
+let y=5.6;
+
+//difference between is var  is like local variable and let is like globle variable var is only use in block where it declere and we can use let everywhere in program. 
+
+console.log(z);
+console.log(v);
+console.log(y);
+
+
+//premetive data-type
+//in java-script there are 7 premetive data-type
+//null,number,string,symbol,undefined,boolean,bigint
+let r=5;
+let s="sjdvb";
+let t=3.6546;
+let u=true;
+let p=undefined;
+let q=null;     //remember that data-type of null is object.//see in output.
+
+console.log(r,s,t,u,p,q);
+console.log(typeof r,typeof s,typeof t,typeof u,typeof p,typeof q);
+
+
+
+
+console.log(z,v,y);
+
+let o = {          //this is call object. we can give any name like vaariable.
+    name: "Milan",                //note that here we cannot give samicolon at the end we give colon 
+    "job code":5319                //here in space between job and code so ""(double cout) is neccesry but in other key that have no space in then ""(double cout) is mendetary.
+}
+//here "name" and "job code" is call key & "Milan" and "5319" is call  value so we can say say that in object we can declare key-value pairs.
---
+++ JAVA SCRIPT/Video-56/script.js
@@ -0,0 +1,51 @@
+//java-script conditional statement and some operator
+
+//arithmatic operator
+//+ ,    -   ,  * ,    /   ,  **    ,   %  , ++   ,  --    
+let a=5;
+let b=2;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a**b);     //"**" is the exponetiation operator.
+console.log(a%b);
+console.log(++a);
+console.log(--b);
+
+//assignment operator
+//= ,    +=  , -=   ,  /=    ,   *=     ,   **=     ,    %=  
+let c=5;
+c+=100;
+
+console.log(c);
+
+//comparison operator
+//==    ,   != ,   ===(equal value and type)   ,   !==(not equal value or not equal type)   ,     >    ,    < ,       >=     ,  <=   ,   ?(tarnary operator)
+
+if("3" === 3)
+{
+    console.log("true");
+}
+else
+{
+    console.log("false");    
+}
+//here we use "===" output is false  because "3"is string but 3 is number so value is same but type is different
+//is we use "3"==3 then it is true
+
+
+
+//logical operator
+//&&    ,   || ,    !(logical not)   
+
+let age = 45;
+
+if(age>18){
+    console.log("you can drive");
+}
+
+else{
+    console.log("you cannot drive");
+}
