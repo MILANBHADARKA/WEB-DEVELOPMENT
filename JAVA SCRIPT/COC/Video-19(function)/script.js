9e87f7577bc9b100e091aed7a7edc9f46a906f17
MILAN BHADARKA <bhadarkamilan3@gmail.com>
2024-02-12 23:16:53 +0530

    12-02-2024

---
+++ JAVA SCRIPT/COC/Video-19(function)/script.js
@@ -0,0 +1,19 @@
+function saymyname()
+{
+    console.log("M");
+    console.log("i");
+    console.log("l");
+    console.log("a");
+    console.log("n");
+}
+
+saymyname();
+console.log("\n");
+
+function add(a,b)
+{
+    return a+b;
+}
+
+let c=add(3,6);
+console.log(c);
