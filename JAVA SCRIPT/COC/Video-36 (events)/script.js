429ec1b14331e2386403984baf442e80bf550434
MILAN BHADARKA <bhadarkamilan3@gmail.com>
2024-02-21 21:59:41 +0530

    21-02-2024

--- JAVA SCRIPT/COC/Video-36 (events)/script.js
+++ JAVA SCRIPT/COC/Video-36 (events)/script.js
@@ -1,4 +1,37 @@
-document.querySelector('#photos').addEventListener('click' , function(e) {
-    let removeit = e.target.parentNode
-    removeit.remove()
+// document.getElementById('tajmahal1').onclick = function () {
+//     alert("TAJMAHAL")
+// }
+//there is no problem but this approch is not right.
+
+// document.getElementById("tajmahal1").addEventListener( 'click' , function (e) {
+//     alert("TAJMAHAL")
+//     this.remove();
+// })
+
+
+//type , timestamp , preventdefault , target , toElement , srcElement , currentTarget ,, clientx , clienty , screenx , screeny
+//altkey , ctrlkey , shiftkey , keycode
+
+document.getElementById("google").addEventListener( 'click' , function (e) {
+    e.preventDefault();     //to change default behevier
 })
+
+
+
+
+document.querySelector('#images').addEventListener('click' , function(e) {
+    // console.log(e.target.parentNode);     //this will give parent of e.target
+
+    // console.log(e.target.tagName);
+
+    if(e.target.tagName === 'IMG')
+    {
+        let removeit = e.target.parentNode
+        removeit.remove()
+    }
+
+    
+    
+})
+
+
