1e73891d0a9c00151347d38fd3f280a15876cf12
MILAN BHADARKA <bhadarkamilan3@gmail.com>
2024-02-28 22:41:59 +0530

    28-02-2024

---
+++ CSS/Video-46  (animation)/2/script.js
@@ -0,0 +1,15 @@
+const divElem = document.querySelector("div");
+const htmlElem = document.querySelector(":root");
+
+htmlElem.addEventListener("click", showHide);
+document.addEventListener("keydown", showHide);
+
+function showHide() {
+  if (divElem.classList[0] === "fade-in") {
+    divElem.classList.remove("fade-in");
+    divElem.classList.add("fade-out");
+  } else {
+    divElem.classList.remove("fade-out");
+    divElem.classList.add("fade-in");
+  }
+}
