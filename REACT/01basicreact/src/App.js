55686edac9787f4fbb4da318b6bfb4cad104a3f9
MILAN BHADARKA <bhadarkamilan3@gmail.com>
2024-04-15 10:02:31 +0530

    15-04-2024

---
+++ REACT/01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Chai or React | Milan</h1>
+  );
+}
+
+export default App;
---
+++ REACT/01vitereact/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+
+  return (
+    <h1>Chai or React with vite | Milan</h1>
+  )
+}
+
+export default App
