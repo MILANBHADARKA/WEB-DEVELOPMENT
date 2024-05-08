1467f678e55413546dd4842cd703b47b103b4e88
MILAN BHADARKA <bhadarkamilan3@gmail.com>
2024-05-08 21:38:35 +0530

    08-05-2024

--- REACT/01basicreact/src/Milan.js
+++
@@ -1,7 +0,0 @@
-function Hello() {
-    return (
-        <h1>Hello Milan</h1>
-    )
-}
-
-export default Hello;
--- REACT/01vitereact/src/Milan.jsx
+++
@@ -1,7 +0,0 @@
-function Hello() {
-    return(
-        <h3>hello from milan.js</h3>
-    )
-}
-
-export default Hello;
